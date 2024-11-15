42220572bebf346ee91f143b58d50c805fc761c7
Juan S. Mrad <juansmrad@gmail.com>
2024-11-14 19:57:35 -0600

    add app.js and readme changes

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Changes from the master branch here.
 
+do change from feature 2
+
 Some change here.
 
 Hello This is version control is awesome
---
+++ app.js
@@ -0,0 +1,13 @@
+
+var example = 'Hello';
+var secret = 'this is a super secret variable value';
+var pageTitle = document.getElementById('pageTitle')
+document.getElementById('pageTitle').innerText = 'Hacked'
+console.log(example);
+
+document.getElementById('number1').addEventListener('change', function (e) {
+  console.log(e);
+  console.log(e.target);
+  console.log(e.target.value);
+  pageTitle.innerText = 'Hacked ' + e.target.value
+})
