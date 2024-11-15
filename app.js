9a97db5a21c1a110c66dcaa67c2ff8da930684d4
Juan S. Mrad <juansmrad@gmail.com>
2024-11-14 20:08:40 -0600

    changes to app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var example = 'Hello';
 var secret = 'this is a super secret variable value';
 var pageTitle = document.getElementById('pageTitle')
 document.getElementById('pageTitle').innerText = 'Hacked'
+
+
 console.log(example);
 
 document.getElementById('number1').addEventListener('change', function (e) {
